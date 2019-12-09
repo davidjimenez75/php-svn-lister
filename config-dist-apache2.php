edfa3993d15c425c2315a23f45c8dc22fc271c33
David Jimenez <davidjimenez75@gmail.com>
2019-12-09 13:12:26 +0100

    new config examples

---
+++ config-dist-apache2.php
@@ -0,0 +1,10 @@
+<?php
+ /*
+  * Config for php-svn-lister
+  *
+  */
+
+$debug = 0;
+$url_svn    = 'http://'.$_SERVER['SERVER_NAME'].'/'; 			    // url to you webserver
+$url_suffix = '';         				                            // add any suffix string to the url?
+$dir        = '/var/www/html';                        	      // folder with svn repos
--- config-dist.php
+++ config-dist.php
@@ -5,6 +5,6 @@
   */
 
 $debug = 0;
-$url_svn   = 'http://localhost/svn/'; 			// url to you webserver
-$url_suffix = '';         				// add any suffix string to the url?
-$dir       = '/var/lib/svn';                        	// folder with svn repos
+$url_svn   = 'http://'.$_SERVER['SERVER_NAME'].'/svn/'; 			// url to you webserver
+$url_suffix = '';         				                            // add any suffix string to the url?
+$dir       = '/var/lib/svn';                        	        // folder with svn repos
