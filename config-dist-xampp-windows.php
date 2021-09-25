2570e50c0632f1acd43d0a85a71ec6a93d97b8b3
davidjimenez75 <davidjimenez75@gmail.com>
2021-09-25 18:40:37 +0200

    Config for XAMPP Windows

---
+++ config-dist-xampp-windows.php
@@ -0,0 +1,10 @@
+<?php
+ /*
+  * Config for php-svn-lister
+  *
+  */
+
+$debug = 0;
+$url_svn    = 'http://';                             	      // url to you webserver
+$url_suffix = '';         				      // add any suffix string to the url?
+$dir        = 'c:/xampp/htdocs';                       	      // folder with svn repos
